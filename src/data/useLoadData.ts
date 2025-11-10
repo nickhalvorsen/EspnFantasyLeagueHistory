41745a6049adb1cbb87fccca27fee9c7d805d4a6
Nick Halvorsen <nick@hugemail.win>
2025-11-10 10:16:33 -0500

    current year fix instead of hard coding 2025

--- src/data/useLoadData.ts
+++ src/data/useLoadData.ts
@@ -3,6 +3,12 @@ import { getAllYears, getYearData } from "./espnApi";
 import { mapAllStats } from "./mapEspnApiResponse";
 import { useStore } from "./useStore";
 
+const tryGetYearData = (year: number) => {
+  try {
+    return getYearData(year);
+  } catch {}
+};
+
 const useLoadData = () => {
   const [isLoading, setIsLoading] = useState(true);
   const setAllData = useStore((state) => state.setAllData);
@@ -11,13 +17,12 @@ const useLoadData = () => {
     const fetchData = async () => {
       const allYears = await getAllYears();
 
-      // temp
-      if (!allYears.includes(2025)) {
-        allYears.push(2025);
-      }
+      // Can't find a way to get a list of all league years. Closest seems to be the league history endpoint which returns a list of all historical years.
+      const nextYear = Math.max(...allYears) + 1;
+      allYears.push(nextYear);
 
       const allYearData = await Promise.all(
-        allYears.map((year) => getYearData(year))
+        allYears.map((year) => tryGetYearData(year)).filter((x) => !!x)
       );
 
       const mapped = mapAllStats(allYearData);
